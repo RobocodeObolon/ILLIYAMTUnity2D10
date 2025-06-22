9d94a63735ca0131852f89b32de9b2ab12586dc6
Linax <jegernayt3330@gmail.com>
2025-06-22 13:59:42 +0300

    aweasd

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
@@ -19,12 +19,14 @@ public class PlayerMovement : MonoBehaviour
         // ќтримуЇмо вх≥д з клав≥атури (в≥с≥ Horizontal ≥ Vertical)
         moveH = Input.GetAxis("Horizontal");
         moveV = Input.GetAxis("Vertical");
+        rb.AddForce(Vector3.up * 20f);
     }
+    
 
     void FixedUpdate()
     {
         // –ух гравц€
-        rb.velocity = new Vector2(moveH * moveSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(moveH * moveSpeed, moveV * moveSpeed);
     }
 }
 
